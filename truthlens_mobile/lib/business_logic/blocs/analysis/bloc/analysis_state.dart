ef00a06bcc6c0e5edf113d9439e24f1110500f2f
Irfan Karim <irfankarimhaji1122@gmail.com>
2025-11-09 00:27:39 +0500

    Analysis Bloc is updated.

--- truthlens_mobile/lib/business_logic/blocs/analysis/bloc/analysis_state.dart
+++ truthlens_mobile/lib/business_logic/blocs/analysis/bloc/analysis_state.dart
@@ -3,4 +3,24 @@ part of 'analysis_bloc.dart';
 @immutable
 sealed class AnalysisState {}
 
-final class AnalysisInitial extends AnalysisState {}
+final class AnalysisInitial extends AnalysisState {
+
+}
+
+final class AnalysisLoading extends AnalysisState {}
+
+final class AnalysisSuccess extends AnalysisState {
+  final String result; // Or a more complex AnalysisResult object
+  AnalysisSuccess(this.result);
+}
+
+final class AnalysisFailure extends AnalysisState {
+  final String error;
+  AnalysisFailure(this.error);
+}
+
+final class AnalysisProgress extends AnalysisState {
+  final double progress; // 0.0 - 1.0
+  AnalysisProgress(this.progress);
+}
+
